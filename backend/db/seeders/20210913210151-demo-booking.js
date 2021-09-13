15f9fbfbfb738be84ee28405cc752297ba0e15ea
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-13 14:18:50 -0700

    fixated demo booking data

--- backend/db/seeders/20210913210151-demo-booking.js
+++ backend/db/seeders/20210913210151-demo-booking.js
@@ -4,8 +4,10 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     let oldDateObj = new Date();
     let newDateObj = new Date();
+    let newDateObj2 = new Date();
 
     newDateObj.setTime(oldDateObj.getTime() + 10080 * 60 * 1000);
+    newDateObj2.setTime(oldDateObj.getTime() + 20160 * 60 * 1000);
 
     return queryInterface.bulkInsert(
       "Bookings",
@@ -28,6 +30,12 @@ module.exports = {
           startDate: oldDateObj,
           endDate: newDateObj,
         },
+        {
+          userId: 4,
+          spotId: 3,
+          startDate: newDateObj,
+          endDate: newDateObj2,
+        },
       ],
       {}
     );
@@ -38,7 +46,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       "Bookings",
       {
-        name: { [Op.in]: ["Bnb on Main", "Bnb on White", "Bnb on Black"] },
+        spotId: { [Op.in]: [3, 1, 2] },
       },
       {}
     );
