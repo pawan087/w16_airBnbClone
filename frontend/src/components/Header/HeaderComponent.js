684b82cbc3e655151203ec306a872a54c6435a01
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-14 11:58:16 -0700

    styled left and middle portion of header, moving onto right

--- frontend/src/components/Header/HeaderComponent.js
+++ frontend/src/components/Header/HeaderComponent.js
@@ -1,4 +1,5 @@
 import styles from "../../components/Header/HeaderComponent.module.css";
+import { SearchIcon } from "react-hero-icon";
 
 export default function HeaderComponent() {
   let imgUrl =
@@ -8,9 +9,26 @@ export default function HeaderComponent() {
       <div>
         <img className={styles.logo} src={imgUrl} alt="airBnbClone-logo"></img>
       </div>
-      <div>
-        <input type="text" placeholder='Search around the world'/>
+      <div className={styles.middleHeader}>
+        <input className={styles.searchInput} type="text" placeholder="Search" />
+        <div className={styles.searchIcon}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
       </div>
+
       <div>Right</div>
     </header>
   );
