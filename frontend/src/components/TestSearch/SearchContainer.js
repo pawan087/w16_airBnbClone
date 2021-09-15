df7d716a7494c898157dd351b094e4d7ef20bb5a
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-15 11:18:13 -0700

    drafted search result

--- frontend/src/components/TestSearch/SearchContainer.js
+++ frontend/src/components/TestSearch/SearchContainer.js
@@ -4,7 +4,7 @@ import { Redirect } from "react-router-dom";
 import { getBookings } from "../../store/bookings";
 import { getSpots } from "../../store/spots";
 import ResultsContainer from "./ResultsContainer";
-
+import styles from "../../components/TestSearch/SearchContainer.module.css";
 export default function SearchContainer() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
@@ -31,6 +31,10 @@ export default function SearchContainer() {
   // console.log(spotsArr[0].Images[0].url); // imgUrl
   // console.log(spotsArr[0].city); // location
 
+  const x = new Date(startDate);
+  const y = new Date(endDate);
+  const dayCount = (y - x) / 60 / 60 / 1000 / 24;
+
   spotsArr.forEach((spot) => {
     if (spot.city === location) {
       searchResultsObj[spot.id] = spot;
@@ -44,23 +48,29 @@ export default function SearchContainer() {
   bookingArr.forEach((booking) => {
     if (booking["Spot"]["city"] === location && startDate && endDate) {
       if (booking.startDate < startDate && endDate < booking.endDate) {
-        searchResultsObj[booking["Spot"]["id"]] = null;
+        // searchResultsObj[booking["Spot"]["id"]] = null;
+        delete searchResultsObj[booking["Spot"]["id"]];
       }
       if (
         startDate < booking.startDate &&
         booking.startDate < endDate &&
         endDate < booking.endDate
       ) {
-        searchResultsObj[booking["Spot"]["id"]] = null;
+        // searchResultsObj[booking["Spot"]["id"]] = null;
+        delete searchResultsObj[booking["Spot"]["id"]];
       }
       if (startDate < booking.startDate && booking.endDate < endDate) {
-        searchResultsObj[booking["Spot"]["id"]] = null;
+        // searchResultsObj[booking["Spot"]["id"]] = null;
+        delete searchResultsObj[booking["Spot"]["id"]];
       }
       if (booking.startDate < startDate && booking.endDate < endDate) {
-        searchResultsObj[booking["Spot"]["id"]] = null;
+        // searchResultsObj[booking["Spot"]["id"]] = null;
+        delete searchResultsObj[booking["Spot"]["id"]];
       }
     }
   });
+
+  console.log(searchResultsObj);
 
   const arr = Object.values(searchResultsObj);
   let today = new Date();
@@ -134,20 +144,75 @@ export default function SearchContainer() {
         <button type="submit">Search</button>
       </form>
 
-      <ResultsContainer startDate={startDate} endDate={endDate} input={location} results={arr}></ResultsContainer>
+      <main>
+        <section>
+          {!arr.length && startDate && endDate && (
+            <p>
+              Sorry, there are no spots available in {location} between{" "}
+              {startDate} and {endDate}.
+            </p>
+          )}
+          {arr.length > 0 && <p>{arr.length} Spot(s) Available</p>}
 
-      <div>
-        {startDate &&
-          endDate &&
-          arr.map((s) => (
-            <div>
-              <a key={s.id} href={`http://localhost:3000/spots/${s.id}`}>
-                {s.name}
-              </a>
-              <img src={s.Images[0].url}></img>
-            </div>
-          ))}
-      </div>
+          {arr.length > 0 && (
+            <h1 className={styles.resultsHeader}>Stays in {location}</h1>
+          )}
+
+          {startDate &&
+            endDate &&
+            arr.map((spot) => (
+              <div className={styles.resultsContainer}>
+                <div className={styles.cardContainer}>
+                  <div className={styles.imgContainer}>
+                    <img
+                      className={styles.img}
+                      layout="fill"
+                      objectFit="cover"
+                      src={spot.Images[0].url}
+                    />
+                  </div>
+
+                  <div className={styles.results}>
+                    <div className={styles.detailContainer}>
+                      <p>{spot.city}</p>
+                      <div className={styles.heartIcon}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+
+                    <h4 className={styles.spotName}>{spot.name}</h4>
+
+                    <div className={styles.divisor} />
+
+                    <p className={styles.detail}>
+                      {spot.address} {spot.city} {spot.country}
+                    </p>
+
+                    <div>
+                      <div>
+                        <p>{spot.price}</p>
+                        <p>TOTAL</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </section>
+      </main>
     </>
   );
 }
