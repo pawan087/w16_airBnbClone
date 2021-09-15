74732bc46c7613235a7d97074f7c1b60ef56f425
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-15 14:44:32 -0700

    finished bookingForm draft

--- frontend/src/components/TestSpot/ReserveFormComponent.js
+++ frontend/src/components/TestSpot/ReserveFormComponent.js
@@ -2,7 +2,9 @@ import styles from "../../components/TestSpot/ReserveFormContainer.module.css";
 export default function ReserveFormComponent() {
   return (
     <div className={styles.outerContainer}>
-      <div className={styles.header}>$489/night</div>
+      <div className={styles.header}>
+        $489<span className={styles.perNight}>/night</span>
+      </div>
       <div className={styles.inputsContainer}>
         <div className={styles.inputContainer}>
           <label className={styles.label}>Check-In</label>
@@ -12,42 +14,42 @@ export default function ReserveFormComponent() {
           <label className={styles.label}>Check-Out</label>
           <input className={styles.dateInput} type="date"></input>
         </div>
-        <div className={styles.inputContainer}>
+        <div className={styles.guestInputContainer}>
           <label className={styles.label}>Guests</label>
           <input className={styles.guestInput} type="number"></input>
         </div>
       </div>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button}>Reserve</button>
+      <div className={styles.btnContainer}>
+        <button className={styles.btn}>Reserve</button>
       </div>
       <div className={styles.detailsContainer}>
         <div className={styles.detailContainer}>
-          <span className={styles.detail}>$515 x 7 nights</span>
-          <span className={styles.detailTotal}>$3,604</span>
+          <div className={styles.detail}>$515 x 7 nights</div>
+          <div className={styles.detailTotal}>$3,604</div>
         </div>
-        <div className={styles.detailsContainer}>
-          <span className={styles.detail}>Weekly discount</span>
-          <span className={styles.detailTotal}>-$180</span>
+        <div className={styles.detailContainer}>
+          <div className={styles.detail}>Weekly discount</div>
+          <div className={styles.detailTotal}>-$180</div>
         </div>
-        <div className={styles.detailsContainer}>
-          <span className={styles.detail}>Cleaning fee</span>
-          <span className={styles.detailTotal}>$275</span>
+        <div className={styles.detailContainer}>
+          <div className={styles.detail}>Cleaning fee</div>
+          <div className={styles.detailTotal}>$275</div>
         </div>
-        <div className={styles.detailsContainer}>
-          <span className={styles.detail}>Service fee</span>
-          <span className={styles.detailTotal}>$522</span>
+        <div className={styles.detailContainer}>
+          <div className={styles.detail}>Service fee</div>
+          <div className={styles.detailTotal}>$522</div>
         </div>
-        <div className={styles.detailsContainer}>
-          <span className={styles.detail}>Occupancy taxes and fees</span>
-          <span className={styles.detailTotal}>$444</span>
+        <div className={styles.detailContainer}>
+          <div className={styles.detail}>Occupancy taxes and fees</div>
+          <div className={styles.detailTotal}>$444</div>
         </div>
       </div>
       <div className={styles.divisorContainer}>
-        <p className={styles.divisor}>_</p>
-      </div>
-      <div className={styles.footer}>
-        <span className={styles.footerDetail}>Total</span>
-        <span className={styles.footerDetail}>$4,665</span>
+        <p className={styles.divisor}></p>
+        <div className={styles.footer}>
+          <div className={styles.footerDetail}>Total</div>
+          <div className={styles.footerDetail}>$4,665</div>
+        </div>
       </div>
     </div>
   );
