c3ac7e74d2d0818f582d91444b27a20d26ce0ac6
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-16 10:57:20 -0700

    able to render bookings without img

--- frontend/src/components/TestBookings/BookingsContainer.js
+++ frontend/src/components/TestBookings/BookingsContainer.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Redirect, Route } from "react-router-dom";
 import { getBookings } from "../../store/bookings";
 import { getSpots } from "../../store/spots";
-
+import styles from "../../components/TestSearch/SearchContainer.module.css";
 export default function BookingsContainer() {
   const dispatch = useDispatch();
   const session = useSelector((state) => state.session);
@@ -27,6 +27,12 @@ export default function BookingsContainer() {
     (booking) => booking.userId === userId
   );
 
+  const dayCount = (startDate, endDate) => {
+    const x = new Date(startDate);
+    const y = new Date(endDate);
+    return (y - x) / 60 / 60 / 1000 / 24;
+  };
+
   return (
     <ul>
       {userBookings.map((booking) => (
@@ -34,6 +40,35 @@ export default function BookingsContainer() {
       ))}
       {userBookings.map((booking) => (
         <li>{booking.startDate}</li>
+      ))}
+      {userBookings.map((booking) => (
+        <div className={styles.resultsContainer}>
+          <div className={styles.cardContainer}>
+            <div className={styles.imgContainer}>
+              <img
+                className={styles.img}
+                layout="fill"
+                objectFit="cover"
+                src={""}
+              />
+            </div>
+            {console.log(booking.endDate - booking.startDate)}
+            <div className={styles.results}>
+              <div className={styles.detailContainer}></div>
+
+              <span className={styles.spotName}>{booking.Spot.name}</span>
+
+              <div className={styles.divisor} />
+
+              <p className={styles.detail}>
+                {" "}
+                {dayCount(booking.startDate, booking.endDate)} night stay: {" "}
+                {booking.startDate.slice(0, 10)} through {" "}
+                {booking.endDate.slice(0, 10)}
+              </p>
+            </div>
+          </div>
+        </div>
       ))}
     </ul>
   );
