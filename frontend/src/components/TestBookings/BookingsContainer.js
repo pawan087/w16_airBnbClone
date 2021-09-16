3b0b5bcbab4b32028fbdd57792d725038724f78d
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-16 11:29:22 -0700

    showing user bookings, working on deleting booking

--- frontend/src/components/TestBookings/BookingsContainer.js
+++ frontend/src/components/TestBookings/BookingsContainer.js
@@ -4,8 +4,11 @@ import { useParams, Redirect, Route } from "react-router-dom";
 import { getBookings } from "../../store/bookings";
 import { getImages } from "../../store/images";
 import { getSpots } from "../../store/spots";
+import { useHistory } from "react-router-dom";
 import styles from "../../components/TestSearch/SearchContainer.module.css";
+import styles2 from "../../components/TestBookings/BookingContainer.module.css";
 export default function BookingsContainer() {
+  const history = useHistory();
   const dispatch = useDispatch();
   const session = useSelector((state) => state.session);
   const bookings = useSelector((state) => state.booking);
@@ -51,16 +54,18 @@ export default function BookingsContainer() {
     return (y - x) / 60 / 60 / 1000 / 24;
   };
 
+  const linkMe = (booking) => {
+    const { spotId } = booking;
+    history.push(`/spots/${spotId}`);
+  };
+
   return (
-    <ul>
+    <>
       {userBookings.map((booking) => (
-        <li>{booking.Spot.name}</li>
-      ))}
-      {userBookings.map((booking) => (
-        <li>{booking.startDate}</li>
-      ))}
-      {userBookings.map((booking) => (
-        <div className={styles.resultsContainer}>
+        <div
+          onClick={() => linkMe(booking)}
+          className={styles2.resultsContainer}
+        >
           <div className={styles.cardContainer}>
             <div className={styles.imgContainer}>
               <img
@@ -84,10 +89,26 @@ export default function BookingsContainer() {
                 {booking.startDate.slice(0, 10)} through{" "}
                 {booking.endDate.slice(0, 10)}
               </p>
+              <div className={styles2.deleteContainer}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
         </div>
       ))}
-    </ul>
+    </>
   );
 }
