b0a9b8c7f7e68addee6fdc74879a9db28adbf53a
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-16 16:41:01 -0700

    edit modal styled, now onto func

--- frontend/src/components/EditBookingModal/EditBookingForm.js
+++ frontend/src/components/EditBookingModal/EditBookingForm.js
@@ -4,11 +4,24 @@ import { getSpots } from "../../store/spots";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../../components/EditBookingModal/EditBookingForm.module.css";
 import { delBooking } from "../../store/bookings";
-
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 function EditBookingForm() {
   const history = useHistory();
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
   return (
     <div className={styles.outerContainer}>
       <div className={styles.logoContainer}>
@@ -23,31 +36,28 @@ function EditBookingForm() {
 
       <div className={styles.cardContainer}>
         <div className={styles.topCardContainer}>
-          <div className={styles.title}>Reservation Cancellation</div>
-          <div className={styles.subtitle}>{"YEE"}</div>
+        <div className={styles.title}>{"Bnb on Daybreak"}</div>
+        <div className={styles.subtitle}>{"Errors:"}</div>
         </div>
         <div className={styles.middleContainer}>
-          <div className={styles.middleHeader}>{"YEE"}'s Itinerary</div>
+          <div className={styles.middleHeader}>{"Pawan"}'s Itinerary</div>
           <div className={styles.divisor}></div>
-          <div className={styles.label}>Date(s)</div>
-          <div className={styles.detail}>{"YEE"}</div>
+
           <div className={styles.label}></div>
           <div className={styles.detail}></div>
-          <div className={styles.middleFooter}>
-            Cancellations, as always, are always free but require a 24 hour
-            notice.
+          <div className={styles.dateRangePickerContainer}>
+            <DateRangePicker
+              className={styles.dateRangePicker}
+              onChange={handleSelect}
+              rangeColors={["#009cd5"]}
+              minDate={new Date()}
+              ranges={[selectionRange]}
+            />
           </div>
         </div>
         <div className={styles.btnsContainer}>
-          <button className={styles.btnCancel}>
-            Go Back
-          </button>
-          <button
-
-            className={styles.btnSubmit}
-          >
-            Cancel Reservation
-          </button>
+          <button className={styles.btnCancel}>Cancel</button>
+          <button className={styles.btnSubmit}>Submit</button>
         </div>
       </div>
     </div>
