28caf46375c1253409fc367836b3857d440adda1
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-19 15:44:08 -0700

    fixed restrictions for logged out user as well as diferentiated between past and future bookings

--- frontend/src/components/TestBookings/BookingsContainer.js
+++ frontend/src/components/TestBookings/BookingsContainer.js
@@ -21,8 +21,19 @@ export default function BookingsContainer() {
   const images = useSelector((state) => state.images);
   const imagesArr = Object.values(images);
   let userBookingsArr = Object.values(bookings);
-
-
+  let pastBookings = [];
+  let futureBookings = [];
+  for (let booking of userBookingsArr) {
+    let today = new Date();
+    let x = new Date(booking.startDate);
+    console.log(x);
+    console.log(x < today);
+    if (x < today) {
+      pastBookings.push(booking);
+    } else {
+      futureBookings.push(booking);
+    }
+  }
 
   useEffect(() => {
     dispatch(getSpots());
@@ -60,71 +71,143 @@ export default function BookingsContainer() {
   };
 
   return (
-    <div className={styles.componentContainer}>
-      {userBookingsArr.length === 0 && <SorryComponent noBookings={true} />}
-      {userBookingsArr.length === 1 && (
-        <h3 className={styles.subHeader}>
-          {userBookingsArr.length} Reservation
-        </h3>
-      )}
-      {userBookingsArr.length > 1 && (
-        <h3 className={styles.subHeader}>
-          {userBookingsArr.length} Reservations
-        </h3>
-      )}
+    <>
+      <div className={styles.componentContainer}>
+        {futureBookings.length === 0 && pastBookings.length === 0 && (
+          <SorryComponent noBookings={true} />
+        )}
+        {futureBookings.length === 1 && (
+          <h3 className={styles.subHeader}>
+            {futureBookings.length} Reservation
+          </h3>
+        )}
+        {futureBookings.length > 1 && (
+          <h3 className={styles.subHeader}>
+            {futureBookings.length} Reservations
+          </h3>
+        )}
 
-      <div className={styles.divisor2} />
-      {userBookingsArr.map((booking) => (
-        <div className={styles2.resultsContainer}>
-          <div className={styles.cardContainer}>
-            <div
-              onClick={() => linkMe(booking)}
-              className={styles2.imgContainer}
-            >
-              <img
-                className={styles.img}
-                layout="fill"
-                objectFit="cover"
-                src={booking.imgUrl}
-              />
-            </div>
-
-            <div className={styles.results}>
-              <div className={styles.detailContainer}></div>
-
-              <div onClick={() => linkMe(booking)} className={styles2.spotName}>
-                {booking.Spot.name}
+        {futureBookings.length !== 0 && <div className={styles.divisor2} />}
+        {futureBookings.map((booking) => (
+          <div className={styles2.resultsContainer}>
+            <div className={styles.cardContainer}>
+              <div
+                onClick={() => linkMe(booking)}
+                className={styles2.imgContainer}
+              >
+                <img
+                  className={styles.img}
+                  layout="fill"
+                  objectFit="cover"
+                  src={booking.imgUrl}
+                />
               </div>
 
-              <div className={styles.divisor} />
+              <div className={styles.results}>
+                <div className={styles.detailContainer}></div>
 
-              <EditBookingModal
-                name={giveMeName(booking.spotId)}
-                username={username}
-                booking={booking}
-              />
+                <div
+                  onClick={() => linkMe(booking)}
+                  className={styles2.spotName}
+                >
+                  {booking.Spot.name}
+                </div>
 
-              <p className={styles2.detail}>
-                {" "}
-                {dayCount(booking.startDate, booking.endDate)} night stay:{" "}
-                {booking.startDate.slice(0, 10)} through{" "}
-                {booking.endDate.slice(0, 10)}
-              </p>
-              <div className={styles.btnContainer}>
-                <CancelBookingConfirmationModal
-                  booking={booking}
-                  bookingId={booking.id}
-                  endDate={booking.endDate}
-                  startDate={booking.startDate}
+                <div className={styles.divisor} />
+
+                <EditBookingModal
                   name={giveMeName(booking.spotId)}
                   username={username}
+                  booking={booking}
                 />
+
+                <p className={styles2.detail}>
+                  {" "}
+                  {dayCount(booking.startDate, booking.endDate)} night stay:{" "}
+                  {booking.startDate.slice(0, 10)} through{" "}
+                  {booking.endDate.slice(0, 10)}
+                </p>
+                <div className={styles.btnContainer}>
+                  <CancelBookingConfirmationModal
+                    booking={booking}
+                    bookingId={booking.id}
+                    endDate={booking.endDate}
+                    startDate={booking.startDate}
+                    name={giveMeName(booking.spotId)}
+                    username={username}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+
+      <div className={styles.componentContainer}>
+        {pastBookings.length === 0 && <SorryComponent noBookings={true} />}
+        {pastBookings.length === 1 && (
+          <h3 className={styles.subHeader}>Previous Reservation</h3>
+        )}
+        {pastBookings.length > 1 && (
+          <h3 className={styles.subHeader}>Previous Reservations</h3>
+        )}
+
+        <div className={styles.divisor2} />
+        {pastBookings.map((booking) => (
+          <div className={styles2.resultsContainer}>
+            <div className={styles.cardContainer}>
+              <div
+                onClick={() => linkMe(booking)}
+                className={styles2.imgContainer}
+              >
+                <img
+                  className={styles.img}
+                  layout="fill"
+                  objectFit="cover"
+                  src={booking.imgUrl}
+                />
+              </div>
+
+              <div className={styles.results}>
+                <div className={styles.detailContainer}></div>
+
+                <div
+                  onClick={() => linkMe(booking)}
+                  className={styles2.spotName}
+                >
+                  {booking.Spot.name}
+                </div>
+
+                <div className={styles.divisor} />
+
+                <EditBookingModal
+                  name={giveMeName(booking.spotId)}
+                  username={username}
+                  booking={booking}
+                />
+
+                <p className={styles2.detail}>
+                  {" "}
+                  {dayCount(booking.startDate, booking.endDate)} night stay:{" "}
+                  {booking.startDate.slice(0, 10)} through{" "}
+                  {booking.endDate.slice(0, 10)}
+                </p>
+                <div className={styles.btnContainer}>
+                  <CancelBookingConfirmationModal
+                    booking={booking}
+                    bookingId={booking.id}
+                    endDate={booking.endDate}
+                    startDate={booking.startDate}
+                    name={giveMeName(booking.spotId)}
+                    username={username}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
