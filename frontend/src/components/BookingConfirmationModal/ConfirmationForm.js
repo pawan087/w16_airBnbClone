793218373d75a2b5509c03abb453500787de5288
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-09-19 17:54:16 -0700

    refactored backend and working on frontend

--- frontend/src/components/BookingConfirmationModal/ConfirmationForm.js
+++ frontend/src/components/BookingConfirmationModal/ConfirmationForm.js
@@ -1,80 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { getBookings } from "../../store/bookings";
-import { getSpots } from "../../store/spots";
 import { useDispatch, useSelector } from "react-redux";
-import styles from "../../components/BookingConfirmationModal/ConfirmationForm.module.css";
-import { Redirect } from "react-router-dom";
+
 import * as bookingActions from "../../store/bookings";
+import { getSpots } from "../../store/spots";
+
+import styles from "../../components/BookingConfirmationModal/ConfirmationForm.module.css";
+
 function ConfirmationForm({ total, spot, startDate, endDate }) {
   const history = useHistory();
-
   const dispatch = useDispatch();
+
   const sessionUser = useSelector((state) => state.session.user);
+
   const [userId, setUserId] = useState(sessionUser.id);
   const [spotId, setSpotId] = useState(spot[0].id);
-  const bookings = useSelector((state) => state.booking);
-  const spots = useSelector((state) => state.spot);
-  const spotsArr = Object.values(spots);
-  const bookingArr = Object.values(bookings);
-  let location = spot[0].city;
-  // let searchResultsObj = {};
 
-  // spotsArr.forEach((spot) => {
-  //   if (spot.city === location) {
-  //     searchResultsObj[spot.id] = spot;
-  //   }
-  // });
-  // bookingArr.forEach((booking) => {
-  //   if (booking["Spot"]["city"] === location && startDate && endDate) {
-  //     if (booking.startDate < startDate && endDate < booking.endDate) {
-  //       // searchResultsObj[booking["Spot"]["id"]] = null;
-  //       delete searchResultsObj[booking["Spot"]["id"]];
-  //     }
-  //     if (
-  //       startDate < booking.startDate &&
-  //       booking.startDate < endDate &&
-  //       endDate < booking.endDate
-  //     ) {
-  //       // searchResultsObj[booking["Spot"]["id"]] = null;
-  //       delete searchResultsObj[booking["Spot"]["id"]];
-  //     }
-  //     if (startDate < booking.startDate && booking.endDate < endDate) {
-  //       // searchResultsObj[booking["Spot"]["id"]] = null;
-  //       delete searchResultsObj[booking["Spot"]["id"]];
-  //     }
-  //     if (booking.startDate < startDate && booking.endDate < endDate) {
-  //       // searchResultsObj[booking["Spot"]["id"]] = null;
-  //       delete searchResultsObj[booking["Spot"]["id"]];
-  //     }
-  //   }
-  // });
-  // const arr = Object.values(searchResultsObj);
   useEffect(() => {
-    dispatch(getBookings());
     dispatch(getSpots());
-    // dispatch(getImages());
   }, [dispatch]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-      dispatch(
-        bookingActions.create({ userId, spotId, startDate, endDate })
-      )
-
-
-
-
-
+    dispatch(bookingActions.create({ userId, spotId, startDate, endDate }));
 
     history.push("/bookings");
-
   };
 
   const cancelMe = () => {
     window.location.reload();
   };
+
   return (
     <div className={styles.outerContainer}>
       <div className={styles.logoContainer}>
@@ -92,12 +49,16 @@ function ConfirmationForm({ total, spot, startDate, endDate }) {
           <div className={styles.title}>Reservation Confirmation</div>
           {spot[0] && <div className={styles.subtitle}>{spot[0].name}</div>}
         </div>
+
         <div className={styles.middleContainer}>
           <div className={styles.middleHeader}>
             {sessionUser.username}'s Itinerary
           </div>
+
           <div className={styles.divisor}></div>
+
           <div className={styles.label}>Date(s)</div>
+
           {startDate && endDate && (
             <div className={styles.detail}>{`${startDate.slice(
               5
@@ -105,6 +66,7 @@ function ConfirmationForm({ total, spot, startDate, endDate }) {
           )}
 
           <div className={styles.label}>Total:</div>
+
           <div className={styles.detail}>
             $
             {total -
@@ -113,14 +75,17 @@ function ConfirmationForm({ total, spot, startDate, endDate }) {
               total * 0.1 +
               total * 0.0625}
           </div>
+
           <div className={styles.middleFooter}>
             The owner will receive payment two days after your stay ends.
           </div>
         </div>
+
         <div className={styles.btnsContainer}>
           <button onClick={cancelMe} className={styles.btnCancel}>
             Cancel
           </button>
+
           <button onClick={handleSubmit} className={styles.btnSubmit}>
             Confirm Reservation
           </button>
